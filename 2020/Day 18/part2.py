a4b828b75313efa7dcbf9f7ca905e9e73a60fabe
Bogdan Paicu <32246391+ZeCactus@users.noreply.github.com>
2020-12-29 11:32:11 +0200

    Day 17

--- 2020/Day 18/part2.py
+++ 2020/Day 18/part2.py
@@ -14,13 +14,17 @@ def evaluate(arr):
             if parantheses == 0:
                 break
         arr[i:j+1] = [evaluate(arr[i+1:j])]
+    i = -1
+    while "+" in arr:
+        i = i + 2
+        if i >= len(arr):
+            break
+        if arr[i] == "+":
+            arr[i-1:i+2] = [arr[i-1] + arr[i+1]]
+            i = i - 2
     while len(arr) > 1:
-        if arr[1] == "+":
-            arr[0:3] = [arr[0] + arr[2]]
-        else:
-            arr[0:3] = [arr[0] * arr[2]]
+        arr[0:3] = [arr[0] * arr[2]]
     return arr[0]
-
 
 sum = 0
 with open("input.txt", "r") as file:
